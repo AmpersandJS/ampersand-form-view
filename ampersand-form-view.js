5ddcbecb1ae966b27b9facd6e8dbe6aa607e27fc
Henrik Joreteg <hjoreteg@gmail.com>
2014-06-09 17:05:02 -0700

    make it based on BBEvents. Add trigger for field changes

--- ampersand-form-view.js
+++ ampersand-form-view.js
@@ -1,3 +1,7 @@
+var BBEvents = require('backbone-events-standalone');
+var extend = require('extend-object');
+
+
 function FormView(opts) {
     this.el = opts.el;
     this.validCallback = opts.validCallback || function () {};
@@ -19,99 +23,103 @@ function FormView(opts) {
     this.checkValid(true);
 }
 
-FormView.prototype.addField = function (fieldView) {
-    this.fields[fieldView.name] = fieldView;
-    this.fieldArray.push(fieldView);
-    if (this.fieldContainerEl) {
-        fieldView.parent = this;
-        fieldView.render();
-        this.fieldContainerEl.appendChild(fieldView.el);
-    }
-};
 
-FormView.prototype.removeField = function (name) {
-    delete this.fields[fieldView.name];
-    this.fieldArray.splice(this.fieldArray.indexOf(field), 1);
-    this.getField(name).remove();
-};
+extend(FormView.prototype, BBEvents, {
+    addField: function (fieldView) {
+        this.fields[fieldView.name] = fieldView;
+        this.fieldArray.push(fieldView);
+        if (this.fieldContainerEl) {
+            fieldView.parent = this;
+            fieldView.render();
+            this.fieldContainerEl.appendChild(fieldView.el);
+        }
+    },
 
-FormView.prototype.getField = function (name) {
-    return this.fields[fieldView.name];
-};
+    removeField: function (name) {
+        delete this.fields[fieldView.name];
+        this.fieldArray.splice(this.fieldArray.indexOf(field), 1);
+        this.getField(name).remove();
+    },
 
-FormView.prototype.setValid = function (now, forceFire) {
-    var prev = this.valid;
-    this.valid = now;
-    if (prev !== now || forceFire) {
-        this.validCallback(now);
-    }
-};
+    getField: function (name) {
+        return this.fields[name];
+    },
 
-FormView.prototype.checkValid = function (forceFire) {
-    var valid = this.fieldArray.every(function (field) {
-        return field.valid;
-    });
-    this.setValid(valid, forceFire);
-    return valid;
-};
+    setValid: function (now, forceFire) {
+        var prev = this.valid;
+        this.valid = now;
+        if (prev !== now || forceFire) {
+            this.validCallback(now);
+        }
+    },
 
-FormView.prototype.beforeSubmit = function () {
-    this.fieldArray.forEach(function (field) {
-        return field.beforeSubmit();
-    });
-};
+    checkValid: function (forceFire) {
+        var valid = this.fieldArray.every(function (field) {
+            return field.valid;
+        });
+        this.setValid(valid, forceFire);
+        return valid;
+    },
 
-FormView.prototype.update = function (field) {
-    // if this one's good check 'em all
-    if (field.valid) {
+    beforeSubmit: function () {
+        this.fieldArray.forEach(function (field) {
+            return field.beforeSubmit();
+        });
+    },
+
+    update: function (field) {
+        this.trigger('change:' + field.name, field);
+        // if this one's good check 'em all
+        if (field.valid) {
+            this.checkValid();
+        } else {
+            this.setValid(false);
+        }
+    },
+
+    remove: function () {
+        this.el.removeEventListener('submit', this.handleSubmit, false);
+        var parent = this.el.parentNode;
+        if (parent) parent.removeChild(this.el);
+        this.fieldArray.forEach(function (field) {
+            field.remove();
+        });
+    },
+
+    handleSubmit: function (e) {
+        this.beforeSubmit();
         this.checkValid();
-    } else {
-        this.setValid(false);
-    }
-};
+        if (!this.valid) {
+            e.preventDefault();
+            return false;
+        }
 
-FormView.prototype.remove = function () {
-    this.el.removeEventListener('submit', this.handleSubmit, false);
-    var parent = this.el.parentNode;
-    if (parent) parent.removeChild(this.el);
-    this.fieldArray.forEach(function (field) {
-        field.remove();
-    });
-};
+        if (this.preventDefault) {
+            e.preventDefault();
+            this.submitCallback(this.getData());
+            return false;
+        }
+    },
 
-FormView.prototype.handleSubmit = function (e) {
-    this.beforeSubmit();
-    this.checkValid();
-    if (!this.valid) {
-        e.preventDefault();
-        return false;
-    }
+    getData: function () {
+        var res = {};
+        for (var key in this.fields) {
+            res[key] = this.fields[key].value;
+        }
+        return this.clean(res);
+    },
 
-    if (this.preventDefault) {
-        e.preventDefault();
-        this.submitCallback(this.getData());
-        return false;
+    render: function () {
+        if (this.rendered) return;
+        if (!this.el) {
+            this.el = document.createElement('form');
+        }
+        if (this.autoAppend) {
+            this.fieldContainerEl = this.el.querySelector('[role=field-container]') || this.el;
+        }
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.el.addEventListener('submit', this.handleSubmit, false);
     }
-};
-
-FormView.prototype.getData = function () {
-    var res = {};
-    for (var key in this.fields) {
-        res[key] = this.fields[key].value;
-    }
-    return this.clean(res);
-};
-
-FormView.prototype.render = function () {
-    if (this.rendered) return;
-    if (!this.el) {
-        this.el = document.createElement('form');
-    }
-    if (this.autoAppend) {
-        this.fieldContainerEl = this.el.querySelector('[role=field-container]') || this.el;
-    }
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.el.addEventListener('submit', this.handleSubmit, false);
-};
+});
 
 module.exports = FormView;
